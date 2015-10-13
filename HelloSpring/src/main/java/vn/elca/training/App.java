fa11ef8263db40d035d5ff662cb42781e8e17a03
ilt <ilentt@ilenlab.com>
2015-10-13 18:02:00 +0700

    update project

--- HelloSpring/src/main/java/vn/elca/training/App.java
+++ HelloSpring/src/main/java/vn/elca/training/App.java
@@ -6,7 +6,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     @SuppressWarnings("resource")
     public static void main(String[] args) {
-        // ApplicationContext context = new ClasPathXmlApplicationContext("beans.xml");
         ApplicationContext contexts = new ClassPathXmlApplicationContext("beans.xml");
         HelloService service = (HelloService) contexts.getBean("helloService");
         Hello h = service.getHello();
